3aef533445561b698b610a25055c00fcc7d07112
Barak Hoffer <barak.hoffer@gmail.com>
2023-04-12 00:54:15 +0300

    change to spice ref module, for filament as param

--- install.sh
+++ install.sh
@@ -8,16 +8,17 @@ tar xf openvaf_23_2_0_linux_amd64.tar.xz
 
 # Download and patch model
 git clone https://github.com/google/skywater-pdk-libs-sky130_fd_pr_reram sky130_fd_pr_reram
-cp sky130_fd_pr_reram/cells/reram_cell/sky130_fd_pr_reram__reram_cell.va ngspice/sky130_fd_pr_reram__reram_cell.va
-patch "ngspice/sky130_fd_pr_reram__reram_cell.va" < "ngspice/va_model_patch"
+cp sky130_fd_pr_reram/cells/reram_cell/sky130_fd_pr_reram__reram_cell.va ngspice/sky130_fd_pr_reram__reram_module.va
+patch "ngspice/sky130_fd_pr_reram__reram_module.va" < "ngspice/va_model_patch"
 
 # Update spice netlist according to env
-sed -i "s|/root/.volare/volare/sky130/versions|$PDK_ROOT|g" ngspice/reram_example.spice
+sed -i "s|/foss/pdks|$PDK_ROOT|g" ngspice/sky130_fd_pr_reram__reram_cell.spice
+sed -i "s|/foss/pdks|$PDK_ROOT|g" ngspice/reram_example.spice
 
 # Compile model
-"$SCRIPT_DIR/openvaf" $SCRIPT_DIR/ngspice/sky130_fd_pr_reram__reram_cell.va
+"$SCRIPT_DIR/openvaf" $SCRIPT_DIR/ngspice/sky130_fd_pr_reram__reram_module.va
 
-# Copy model and example netlist
+# Copy model, spice file and example netlist
 cp "ngspice"/*.va ${PDK_ROOT}/sky130B/libs.tech/ngspice
 cp "ngspice"/*.spice ${PDK_ROOT}/sky130B/libs.tech/ngspice
 cp "ngspice"/*.osdi ${PDK_ROOT}/sky130B/libs.tech/ngspice
